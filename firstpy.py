baef05850678a08dd92d33aeb7058d85678cde72
Zachary R.Fang <fangziwer@gmail.com>
2018-10-19 10:50:37 +0800

    fang contribute to lcy
    fang added lines from line 6 to line 14
    contribute to LCY

--- firstpy.py
+++ firstpy.py
@@ -3,3 +3,12 @@ for i in range(10):
 
 a=[x for x in range(1000) if x%2==0]
 print(a)
+
+start_list = [5, 3, 1, 2, 4]
+square_list = []
+
+# Your code here!
+for number in start_list:
+	start_list.append(number)
+
+print (start_list)
